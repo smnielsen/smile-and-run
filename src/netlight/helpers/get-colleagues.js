83aad95ff6c787b76372fff833cec2ba991f7652
Simon <simonnielsen@live.se>
2021-04-13 18:25:08 +0200

    fixes

--- src/netlight/helpers/get-colleagues.js
+++ src/netlight/helpers/get-colleagues.js
@@ -1,43 +1,43 @@
-const assert = require('assert');
-const fs = require('fs').promises;
-const path = require('path');
-const axios = require('axios');
-const inquirer = require('inquirer');
-const getCookieAuth = require('./get-cookie-auth');
-const log = require('../../util/logger').create({ name: 'colleagues' });
-const config = require('../../config');
+const assert = require('assert')
+const fs = require('fs').promises
+const path = require('path')
+const axios = require('axios')
+const inquirer = require('inquirer')
+const getCookieAuth = require('./get-cookie-auth')
+const log = require('../../util/logger').create({ name: 'colleagues' })
+const config = require('../../config')
 
 const createCache = async () => {
-  const filepath = path.resolve(config.outputDir, `input-mentors.json`);
+  const filepath = path.resolve(config.outputDir, `input-mentors.json`)
 
   const getCache = async () => {
     try {
-      const res = await fs.readFile(filepath);
-      return JSON.parse(res).data;
+      const res = await fs.readFile(filepath)
+      return JSON.parse(res).data
     } catch (err) {
-      log.warn(`Could not read cache ${err.message}`.yellow);
-      return null;
+      log.warn(`Could not read cache ${err.message}`.yellow)
+      return null
     }
-  };
+  }
 
-  const writeCache = async data => {
+  const writeCache = async (data) => {
     try {
-      await fs.writeFile(filepath, JSON.stringify(data));
+      await fs.writeFile(filepath, JSON.stringify(data))
     } catch (err) {
-      log.warn(`Could not write cache ${err.message}`.yellow);
-      return false;
+      log.warn(`Could not write cache ${err.message}`.yellow)
+      return false
     }
-    return true;
-  };
+    return true
+  }
   return {
     read: getCache,
     write: writeCache,
-  };
-};
+  }
+}
 
 module.exports = async () => {
-  const cache = await createCache();
-  const cacheData = await cache.read();
+  const cache = await createCache()
+  const cacheData = await cache.read()
 
   if (cacheData) {
     const { useCache } = await inquirer.prompt([
@@ -47,18 +47,19 @@ module.exports = async () => {
         message: 'Colleagues cache exists, use cache?',
         choices: ['yes', 'no'],
       },
-    ]);
+    ])
     if (useCache === 'yes') {
-      return cacheData;
+      return cacheData
     }
   }
 
-  log('>> Getting colleagues from API');
-  const authCookie = await getCookieAuth();
-  assert(authCookie, 'Missing authcookie');
+  log('>> Getting colleagues from API')
+  const authCookie = await getCookieAuth()
+  console.log('authCookie', authCookie)
+  assert(authCookie, 'Missing authcookie')
 
   const url =
-    'https://rebel.netlight.com/wp-admin/admin-ajax.php?action=contact_list';
+    'https://rebel.netlight.com/wp-admin/admin-ajax.php?action=contact_list'
 
   const res = await axios({
     method: 'get',
@@ -67,23 +68,23 @@ module.exports = async () => {
       'Content-Type': 'application/x-www-form-urlencoded',
       Cookie: authCookie,
     },
-  });
+  })
 
   if (res.status > 299) {
-    throw new Error(`Could not get colleagues: ${res.statusText}`);
+    throw new Error(`Could not get colleagues: ${res.statusText}`)
   }
 
   const leftNetlight = [
     'lucrece.rolland@netlight.com',
     'anthon.johansson@netlight.com',
     'philipp.grassinger@netlight.com',
-  ];
+  ]
   const netlighters = res.data.data.filter(
     ([, email]) => !leftNetlight.includes(email),
-  );
+  )
 
   // Store cache
-  await cache.write({ data: netlighters });
+  await cache.write({ data: netlighters })
 
-  return netlighters;
-};
+  return netlighters
+}
